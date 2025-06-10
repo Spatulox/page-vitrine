8a66e94336a60ceac526f09330475480d209223e
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-10 11:06:17 +0200

    add legal

--- src/pages/Legal/Legal.tsx
+++ src/pages/Legal/Legal.tsx
@@ -1,5 +1,10 @@
-function Legal(){
-    return <h1>Legal</h1>
+function Legal() {
+    return (
+        <div>
+            <h1>Mentions Légales</h1>
+            <p>Ce site est édité par La Maison Horrifique - 2025.</p>
+        </div>
+    )
 }
 
 export default Legal
