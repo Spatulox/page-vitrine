a9557e4dbc0d3b00675203217060d4545ad42ab3
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-10 10:50:07 +0200

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,19 @@
-function Footer(){
-    return <div><h1>Footer</h1></div>
+import {Link} from 'react-router-dom';
+
+function Footer() {
+    return (
+    <footer>
+        <p>Suivez-nous :
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"> Facebook</a> |
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"> Instagram</a>
+        </p>
+        <p>
+            <Link to="/sessions">Détails des sessions</Link> |{" "}
+            <Link to="/contact">Contact</Link> |{" "}
+            <Link to="/legal">Mentions légales</Link>
+        </p>
+    </footer>
+    )
 }
 
-export default Footer
+export default Footer;
