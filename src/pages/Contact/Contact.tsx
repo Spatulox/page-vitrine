d756567fec9cfbea99acdfea9809974f0c411e2f
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-10 11:03:21 +0200

    add Contact

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,15 @@
-function Contact(){
-    return <h1>Contact</h1>
+function Contact() {
+    return (
+        <div>
+            <h1>Contactez-nous</h1>
+            <form>
+                <input type="text" placeholder="Nom" required/><br/>
+                <input type="email" placeholder="Email" required/><br/>
+                <textarea placeholder="Votre message..." required></textarea><br/>
+                <button type="submit">Envoyer</button>
+            </form>
+        </div>
+    )
 }
 
 export default Contact
