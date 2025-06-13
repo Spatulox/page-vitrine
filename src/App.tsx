229bc360895424c8718700db6523d691279d18ac
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-06-13 17:23:28 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
         <Header />
-        <main>
+        <main className="container">
         <Routes>
           <Route path={"/"} element={<Accueil />}></Route>
           <Route path={"/accueil"} element={<Accueil />}></Route>
