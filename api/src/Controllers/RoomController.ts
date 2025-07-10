4ba2cedb20196b4d08c0d52236b97207dfe2f9c5
Spatulox <M78stormtrooper@laposte.net>
2025-07-10 11:57:16 +0200

    Adds room sessions endpoint
    Adds an endpoint to retrieve sessions associated with a specific room.
    Implements data models for session representation.
    Validates request parameters for retrieving room sessions.

--- api/src/Controllers/RoomController.ts
+++ api/src/Controllers/RoomController.ts
@@ -1,8 +1,11 @@
-import { Get, JsonController, Param } from "routing-controllers";
+import { Get, JsonController, Param, QueryParams } from "routing-controllers";
 import { FilledRoom } from "../Models/RoomModel";
 import { ObjectID } from "../DB_Schema/connexion";
 import { zObjectId } from "../Validators/utils";
 import { getAllRooms, getRoomById } from "../Services/rooms/rooms";
+import { zGetRoomParams } from "../Validators/rooms";
+import { getEmptySessionsRoomById, getSessionsRoomById } from "../Services/sessions/sessions";
+import { RoomSessions, RoomSessionsEmpty } from "../Models/SessionsModel";
 
 @JsonController("/rooms")
 export class RoomController {
@@ -15,5 +18,12 @@ export class RoomController {
   async getRoomById(@Param('id') user_id: ObjectID): Promise<FilledRoom | null> {
     const validId = zObjectId.parse(user_id)
     return await getRoomById(new ObjectID(validId));
+  }
+
+  @Get('/:id/sessions')
+  async getSessionsByRoomById(@Param('id') user_id: ObjectID, @QueryParams() param: any): Promise<RoomSessionsEmpty> {
+    const validId = zObjectId.parse(user_id)
+    const validParam = zGetRoomParams.parse(param)
+    return await getEmptySessionsRoomById(new ObjectID(validId), validParam);
   }
 }
--- api/src/Models/SessionsModel.ts
+++ api/src/Models/SessionsModel.ts
@@ -1,8 +1,23 @@
 import { ObjectID } from "../DB_Schema/connexion"
+import { FilledRoom } from "./RoomModel";
+import { FilledUser } from "./UserModel";
 
 export type Sessions = {
     _id: ObjectID,
     room_id: ObjectID,
     user_id: ObjectID,
     start_time: Date,
+    participants: ObjectID[],
+}
+
+export type FilledSessions = Omit<Sessions, "_id" | "room_id" | "user_id"> & {_id: string, room: FilledRoom, user: FilledUser | null};
+
+export type RoomSessions = {
+    room_id: string,
+    sessions: FilledSessions[]
+}
+
+export type RoomSessionsEmpty = {
+    room: string,
+    free_sessions: string[]
 }
