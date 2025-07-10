b6491e802224219a2f92bddd7501ef9416255326
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-07-10 09:21:50 +0200

    Create Endpoint route enum
    Adds an enum to store the application's API endpoint routes.

---
+++ front/src/api/Endpoint.ts
@@ -0,0 +1,14 @@
+export enum EndpointRoute {
+    /* AUTH */
+    login = "/auth/login",
+    
+    
+    /* USERS */
+    adminUser = "/admin/users",
+    users = "/users",
+    me = "/users/@me",
+
+    /* ROOMS */
+    rooms = "/rooms"
+
+}
