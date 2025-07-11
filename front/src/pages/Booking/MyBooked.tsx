d09b7ae547e1901dec08bb0dab07dc2dd800551a
Spatulox <M78stormtrooper@laposte.net>
2025-07-11 15:39:49 +0200

    Add MySessions Components

--- front/src/pages/Booking/MyBooked.tsx
+++ front/src/pages/Booking/MyBooked.tsx
@@ -1,7 +1,66 @@
-export function MyActiveBook(){
-    return <h1>Current book</h1>
+import { useEffect, useState } from "react"
+import { GetApi } from "../../api/Axios"
+import { EndpointRoute } from "../../api/Endpoint"
+import type { Sessions } from "../../api/Sessions"
+import { SessionsList } from "./BookList"
+import NotFound from "../../components/NotFound"
+
+export function MySessions(){
+    return <>
+    <MyActiveBook/>
+    <MyOldBook/>
+    </>
 }
 
-export function MyOldBook(){
-    return <h1>Old book</h1>
+
+function MyActiveBook(){
+    const [book, setBook] = useState<Sessions[]>()
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await GetApi(`${EndpointRoute.book}`)
+                setBook(res)
+            } catch (error) {
+                console.error(error)
+            }
+        })()
+    },[])
+
+    if(!book){
+        return <NotFound />
+    }
+
+    return<>
+        <div>
+            <h1>Current book</h1>
+            <SessionsList sessions={book}/>
+        </div>
+    </>
+}
+
+function MyOldBook(){
+    const [book, setBook] = useState<Sessions[]>()
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await GetApi(`${EndpointRoute.book}/historic`)
+                setBook(res)
+            } catch (error) {
+                console.error(error)
+            }
+        })()
+    },[])
+
+    if(!book){
+        return <NotFound />
+    }
+
+    return<>
+        <div>
+            <h1>Old book</h1>
+            <SessionsList sessions={book}/>
+        </div>
+    </>
 }
